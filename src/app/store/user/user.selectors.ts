3a2da72f231b7a1b564c70eb36eaf766c7611e98
Ben Aves <avesbenjamin@gmail.com>
2017-04-08 17:27:23 +1000

    fix user selctor bug

--- src/app/store/user/user.selectors.ts
+++ src/app/store/user/user.selectors.ts
@@ -4,5 +4,5 @@ import { AngularFire } from 'angularfire2';
 export namespace UserSelectors {
 
     export const IsPasswordUser = (firebase: AngularFire) =>
-        firebase.auth.map(auth => auth.provider === AuthProviders.Password);
+        firebase.auth.map(auth => !!auth && auth.provider === AuthProviders.Password);
 }
