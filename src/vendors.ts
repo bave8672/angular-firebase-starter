0b0ea5d1336f4880768fc6a84fcc908b2509b0eb
Ben Aves <ben.aves@ir.com>
2018-02-26 23:08:04 +1100

    fix import

--- src/vendors.ts
+++ src/vendors.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/first';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/switchmap';
+import 'rxjs/add/operator/switchMap';
 
 import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/race';
